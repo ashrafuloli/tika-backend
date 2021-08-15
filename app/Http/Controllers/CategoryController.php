d726478aad9125ae59b0ef44986de0356c2ff8b5
Ashraful Oli <rock.ashraful@gmail.com>
2021-08-15 18:42:16 +0600

    update

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -7,25 +7,18 @@ use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
+
     /**
      * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
      */
     public function index()
     {
-<<<<<<< HEAD
         $categories = Category::paginate();
         return view('categories.index', ['categories' => $categories]);
-=======
-        //
->>>>>>> origin/master
     }
 
     /**
      * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
      */
     public function create()
     {
@@ -35,74 +28,47 @@ class CategoryController extends Controller
     /**
      * Store a newly created resource in storage.
      *
-<<<<<<< HEAD
-     * @param \Illuminate\Http\Request $request
-=======
-     * @param  \Illuminate\Http\Request  $request
->>>>>>> origin/master
-     * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
     {
         //
     }
 
+
     /**
      * Display the specified resource.
-     *
-<<<<<<< HEAD
-     * @param \App\Models\Category $category
-=======
-     * @param  \App\Models\Category  $category
->>>>>>> origin/master
-     * @return \Illuminate\Http\Response
+     * @param Category $category
      */
     public function show(Category $category)
     {
         //
     }
 
+
     /**
      * Show the form for editing the specified resource.
-     *
-<<<<<<< HEAD
-     * @param \App\Models\Category $category
-=======
-     * @param  \App\Models\Category  $category
->>>>>>> origin/master
-     * @return \Illuminate\Http\Response
+     * @param Category $category
      */
     public function edit(Category $category)
     {
         //
     }
 
+
     /**
      * Update the specified resource in storage.
-     *
-<<<<<<< HEAD
-     * @param \Illuminate\Http\Request $request
-     * @param \App\Models\Category $category
-=======
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Category  $category
->>>>>>> origin/master
-     * @return \Illuminate\Http\Response
+     * @param Request $request
+     * @param Category $category
      */
     public function update(Request $request, Category $category)
     {
         //
     }
 
+
     /**
      * Remove the specified resource from storage.
-     *
-<<<<<<< HEAD
-     * @param \App\Models\Category $category
-=======
-     * @param  \App\Models\Category  $category
->>>>>>> origin/master
-     * @return \Illuminate\Http\Response
+     * @param Category $category
      */
     public function destroy(Category $category)
     {
